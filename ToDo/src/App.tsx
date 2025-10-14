80f5575fe687db0288454aadafe212acc95deeae
Manu-Alriche <e.alriche@yahoo.com>
2025-10-14 16:27:35 +0200

    feat: initialize ToDo application with React, TypeScript, and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Created main application structure including App component and TodoItems component.
    - Implemented local storage functionality for managing todos.
    - Added filtering capabilities for todos based on priority.
    - Styled components using Tailwind CSS and DaisyUI.
    - Configured TypeScript with appropriate settings for the application.
    - Set up Vite for development and build processes.

---
+++ ToDo/src/App.tsx
@@ -0,0 +1,149 @@
+import { useState, useEffect } from "react";
+import TodoItems from "./components/TodoItems";
+import { Construction } from "lucide-react";
+
+type Priority = "Basse" | "Moyenne" | "Urgente";
+type Todo = {
+  id: number;
+  text: string;
+  priority: Priority;
+};
+
+function App() {
+  const [input, setInput] = useState<string>("");
+  const [priority, setPriority] = useState<Priority>("Moyenne");
+
+  const savedTodos = localStorage.getItem("todos");
+  const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+  const [filter, setFilter] = useState<Priority | "Tous">("Tous");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo() {
+    if (input.trim() == "") {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: input.trim(),
+      priority: priority,
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    setInput("");
+    setPriority("Moyenne");
+  }
+
+  let filteredTodos: Todo[] = [];
+  if (filter === "Tous") {
+    filteredTodos = todos;
+  } else {
+    filteredTodos = todos.filter((todo) => todo.priority === filter);
+  }
+
+  const urgentCount = todos.filter(
+    (todo) => todo.priority === "Urgente"
+  ).length;
+  const mediumCount = todos.filter(
+    (todo) => todo.priority === "Moyenne"
+  ).length;
+  const lowCount = todos.filter((todo) => todo.priority === "Basse").length;
+  const totalCount = todos.length;
+
+  function deleteTodo(id: number) {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col gap64 my-15 bg-basse-300 p-5 rounded-2xl">
+        <div className="flex gap-4">
+          <input
+            type="text"
+            placeholder="Ajouter une tâche"
+            className="input w-full flex-grow p-2 mb-4 border border-gray-300 rounded"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <select
+            aria-label="Priorité de la tâche"
+            className="select w-full p-2 border border-gray-300 rounded"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as Priority)}
+          >
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Basse</option>
+          </select>
+          <button onClick={addTodo} className="btn btn-primary ">
+            Ajouter
+          </button>
+        </div>
+        <div className="space-y-2 flex-1 h-fit">
+          <div className="flex flex-wrap gap-4">
+            <button
+              className={`btn btn-soft ${
+                filter === "Tous" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Tous")}
+            >
+              Tous ({totalCount})
+            </button>
+            <button
+              className={`btn btn-soft ${
+                filter === "Tous" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Urgente")}
+            >
+              Urgente ({urgentCount})
+            </button>
+            <button
+              className={`btn btn-soft ${
+                filter === "Tous" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Moyenne")}
+            >
+              Moyenne ({mediumCount})
+            </button>
+            <button
+              className={`btn btn-soft ${
+                filter === "Tous" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Basse")}
+            >
+              Basse ({lowCount})
+            </button>
+          </div>
+
+          {filteredTodos.length > 0 ? (
+            <ul className="divide-y divide-primary/20">
+              {filteredTodos.map((todo) => (
+                <li key={todo.id}>
+                  <TodoItems todo={todo} onDelete={() => deleteTodo(todo.id)} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="flex justify-center items-center flex-col p-5">
+              <div>
+                <Construction
+                  strokeWidth={1}
+                  className="w-40 h-40 text-primary"
+                />
+              </div>
+              <p className="text-sm">Aucune tâche pour ce filtre</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ ToDo/src/components/TodoItems.tsx
@@ -0,0 +1,49 @@
+import { Trash } from "lucide-react";
+
+type Priority = "Basse" | "Moyenne" | "Urgente";
+type Todo = {
+  id: number;
+  text: string;
+  priority: Priority;
+};
+type Props = {
+  todo: Todo;
+  onDelete: () => void;
+};
+
+const TodoItems = ({ todo, onDelete }: Props) => {
+  return (
+    <li className="p-3">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            className="checkbox checkbox-primary checkbox-sm"
+          />
+          <span className="text-md font-bold"></span>
+          <span className="text-md">{todo.text}</span>
+          <span
+            className={`badge badge-sm badge-soft ${
+              todo.priority === "Urgente"
+                ? "badge-error"
+                : todo.priority === "Moyenne"
+                ? "badge-warning"
+                : "badge-success"
+            }`}
+          >
+            {todo.priority}
+          </span>
+        </div>
+        <button
+          onClick={onDelete}
+          className="btn btn-sm btn-error btn-soft"
+          title="Delete todo"
+        >
+          <Trash className="w-4 h-4"></Trash>
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItems;
